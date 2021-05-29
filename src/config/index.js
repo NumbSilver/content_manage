3d9eec05ec24dd31cbb35157a9391c1ce5c1de2d
NumbSilver <874409827@qq.com>
2021-05-29 21:57:23 +0800

    modal框出问题

--- src/config/index.js
+++ src/config/index.js
@@ -8,3 +8,6 @@ export const WEATHER_ak = '3p49MVra6urFRGOT9s8UBWr2'
 
 //城市名
 export const CITY = 'beijing'
+
+// 每页展示几条
+export const PAGE_SIZE = 10
--- src/containers/category/category.jsx
+++ src/containers/category/category.jsx
@@ -1,11 +1,59 @@
-import React,{Component} from 'react'
-// import './**.css'
+import React,{Component,useState} from 'react'
+import './css/category.less'
+import {Card,Button,Table,message,Modal} from 'antd'
+import {PlusOutlined} from '@ant-design/icons'
+
+import {reqCatgoryList} from '../../api'
+import {PAGE_SIZE} from '../../config/index'
 export default class Category extends Component{
- render(){
-    return (
-      <div>
-          Category
-      </div>
-    )
+  state={
+    // 商品分类列表
+    categoryList:[],
+    
   }
+  componentDidMount(){
+    this.getCategoryList()
+    // const [isModalVisible, setIsModalVisible] = useState(false);
+  }
+  getCategoryList= async()=>{
+    let result = await reqCatgoryList()
+    let{status,data,msg} = result
+
+    if(status==0){
+      this.setState({categoryList:data})
+    }else{
+      message.error(msg,1)
+    }
+  }
+g
+  render(){
+    const dataSource = this.state.categoryList;
+    const columns = [
+      {
+        title: '分类名',
+        dataIndex: 'name',
+        key: 'name',
+      },
+      {
+        title: '操作',
+        // dataIndex: 'key', 当动态传参的时候，写谁传谁
+        key: 'todo',
+        render:()=>{return <Button type="link" onClick={this.showModal}>添加</Button>},
+        width:'25%',
+        align:'center',
+      }
+    ];
+    
+      return (
+        <div>
+          <Card className="category" title="" extra={<Button onClick={this.showModal} type="primary" icon={<PlusOutlined />}>修改分类</Button>}>  
+            <Table dataSource={dataSource} columns={columns} 
+                  bordered rowKey="_id" pagination={{pageSize:PAGE_SIZE}}/>
+          </Card>
+          <Modal title="Basic Modal" visible={this.isModalVisible} onOk={this.handleOk} onCancel={this.handleCancel}>
+            <p>Some contents...</p>
+          </Modal>
+        </div>
+      )
+    }
 }
